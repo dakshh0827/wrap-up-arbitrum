ead1e029dc9b632ccdb665c52f44d85f98df0c01
Daksh Thakran <daksh.thakran05@gmail.com>
2026-01-15 12:02:05 +0530

    Update wagmiConfig.js

--- frontend/src/wagmiConfig.js
+++ frontend/src/wagmiConfig.js
@@ -1,6 +1,9 @@
+wagmi config will be:
+
+
 // File: src/wagmiConfig.js
 import { http, createConfig } from 'wagmi'
-import { mantleSepoliaTestnet } from 'wagmi/chains'
+import { arbitrumSepolia } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 
 import { createWeb3Modal } from '@web3modal/wagmi/react';
@@ -13,46 +16,21 @@ if (!projectId) {
 }
 
 // 2. Define Chains
-// We define Mantle Sepolia here so it can be used in the config
-export const mantleSepolia = {
-  id: 5003,
-  name: 'Mantle Sepolia',
-  nativeCurrency: { name: 'Mantle', symbol: 'MNT', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://rpc.sepolia.mantle.xyz'] },
-  },
-  blockExplorers: {
-    default: { name: 'Mantle Explorer', url: 'https://explorer.sepolia.mantle.xyz' },
-  },
-  testnet: true,
-};
+// Use Arbitrum Sepolia as the active chain
+export const activeChain = arbitrumSepolia;
 
-// export const monadTestnet = {
-//   id: 10143,
-//   name: 'Monad Testnet',
-//   nativeCurrency: { name: 'Monad', symbol: 'MONAD', decimals: 18 },
-//   rpcUrls: {
-//     default: { http: ['https://testnet-rpc.monad.xyz/'] },
-//   },
-//   blockExplorers: {
-//     default: { name: 'MonadScan', url: 'https://testnet-explorer.monad.xyz' },
-//   },
-//   testnet: true,
-// };
-
-// *** CRITICAL FIX: Add mantleSepolia to this array ***
-const chains = [mantleSepolia]; 
+const chains = [activeChain];
 
 // 3. Create wagmiConfig
 const metadata = {
-  name: 'MonadFeed',
-  description: 'A Decentralised Web3 News Curation Platform',
+  name: 'Wrap-Up', // Updated branding
+  description: 'A Decentralised Web3 News Curation Platform on Arbitrum',
   url: 'https://monadfeed.xyz', 
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
 export const wagmiConfig = defaultWagmiConfig({
-  chains, // This now includes Mantle
+  chains,
   projectId,
   metadata,
   enableWalletConnect: true,
@@ -65,7 +43,7 @@ createWeb3Modal({
   wagmiConfig,
   projectId,
   chains,
-  defaultChain: mantleSepolia, // Optional: Force default to Mantle
+  defaultChain: activeChain,
   themeMode: 'dark',
   themeVariables: {
     '--w3m-accent': '#E79B04',
@@ -75,7 +53,7 @@ createWeb3Modal({
 
 // 5. Export Contract Details & ABIs
 
-// Import ABIs (Pasted exactly as provided)
+// Import ABIs (Kept exactly as provided)
 const MonadFeedABI = {"abi":[{"type":"function","name":"articleComments","inputs":[{"name":"","type":"uint256","internalType":"uint256"},{"name":"","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"articleCount","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"articles","inputs":[{"name":"","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"ipfsHash","type":"string","internalType":"string"},{"name":"curator","type":"address","internalType":"address"},{"name":"upvoteCount","type":"uint256","internalType":"uint256"},{"name":"timestamp","type":"uint256","internalType":"uint256"},{"name":"exists","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"commentCount","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"comments","inputs":[{"name":"","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"ipfsHash","type":"string","internalType":"string"},{"name":"articleId","type":"uint256","internalType":"uint256"},{"name":"commenter","type":"address","internalType":"address"},{"name":"upvoteCount","type":"uint256","internalType":"uint256"},{"name":"timestamp","type":"uint256","internalType":"uint256"},{"name":"exists","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"displayNames","inputs":[{"name":"","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"getArticle","inputs":[{"name":"_articleId","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"ipfsHash","type":"string","internalType":"string"},{"name":"curator","type":"address","internalType":"address"},{"name":"upvoteCount","type":"uint256","internalType":"uint256"},{"name":"timestamp","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"getArticleComments","inputs":[{"name":"_articleId","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"uint256[]","internalType":"uint256[]"}],"stateMutability":"view"},{"type":"function","name":"getArticlesBatch","inputs":[{"name":"_startId","type":"uint256","internalType":"uint256"},{"name":"_count","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"ids","type":"uint256[]","internalType":"uint256[]"},{"name":"curators","type":"address[]","internalType":"address[]"},{"name":"upvoteCounts","type":"uint256[]","internalType":"uint256[]"},{"name":"timestamps","type":"uint256[]","internalType":"uint256[]"}],"stateMutability":"view"},{"type":"function","name":"getComment","inputs":[{"name":"_commentId","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"ipfsHash","type":"string","internalType":"string"},{"name":"articleId","type":"uint256","internalType":"uint256"},{"name":"commenter","type":"address","internalType":"address"},{"name":"upvoteCount","type":"uint256","internalType":"uint256"},{"name":"timestamp","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"getDisplayName","inputs":[{"name":"_user","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"getPlatformStats","inputs":[],"outputs":[{"name":"totalArticles","type":"uint256","internalType":"uint256"},{"name":"totalComments","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"getUserPoints","inputs":[{"name":"_user","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"hasUpvotedArticle","inputs":[{"name":"","type":"address","internalType":"address"},{"name":"","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"hasUpvotedComment","inputs":[{"name":"","type":"address","internalType":"address"},{"name":"","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"hasUserUpvotedArticle","inputs":[{"name":"_user","type":"address","internalType":"address"},{"name":"_articleId","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"hasUserUpvotedComment","inputs":[{"name":"_user","type":"address","internalType":"address"},{"name":"_commentId","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"postComment","inputs":[{"name":"_articleId","type":"uint256","internalType":"uint256"},{"name":"_ipfsHash","type":"string","internalType":"string"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"setDisplayName","inputs":[{"name":"_newName","type":"string","internalType":"string"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"submitArticle","inputs":[{"name":"_ipfsHash","type":"string","internalType":"string"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"upvoteArticle","inputs":[{"name":"_articleId","type":"uint256","internalType":"uint256"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"upvoteComment","inputs":[{"name":"_commentId","type":"uint256","internalType":"uint256"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"userPoints","inputs":[{"name":"","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"event","name":"ArticleSubmitted","inputs":[{"name":"articleId","type":"uint256","indexed":true,"internalType":"uint256"},{"name":"ipfsHash","type":"string","indexed":false,"internalType":"string"},{"name":"curator","type":"address","indexed":true,"internalType":"address"},{"name":"timestamp","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"ArticleUpvoted","inputs":[{"name":"articleId","type":"uint256","indexed":true,"internalType":"uint256"},{"name":"voter","type":"address","indexed":true,"internalType":"address"},{"name":"curator","type":"address","indexed":true,"internalType":"address"},{"name":"newUpvoteCount","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"CommentPosted","inputs":[{"name":"articleId","type":"uint256","indexed":true,"internalType":"uint256"},{"name":"commentId","type":"uint256","indexed":true,"internalType":"uint256"},{"name":"ipfsHash","type":"string","indexed":false,"internalType":"string"},{"name":"commenter","type":"address","indexed":true,"internalType":"address"},{"name":"timestamp","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"CommentUpvoted","inputs":[{"name":"commentId","type":"uint256","indexed":true,"internalType":"uint256"},{"name":"articleId","type":"uint256","indexed":true,"internalType":"uint256"},{"name":"voter","type":"address","indexed":true,"internalType":"address"},{"name":"commenter","type":"address","indexed":false,"internalType":"address"},{"name":"newUpvoteCount","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"DisplayNameSet","inputs":[{"name":"user","type":"address","indexed":true,"internalType":"address"},{"name":"displayName","type":"string","indexed":false,"internalType":"string"}],"anonymous":false},{"type":"event","name":"PointsAwarded","inputs":[{"name":"user","type":"address","indexed":true,"internalType":"address"},{"name":"pointsEarned","type":"uint256","indexed":false,"internalType":"uint256"},{"name":"totalPoints","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false}]};
 
 const MFDTokenABI = {"abi":[{"type":"constructor","inputs":[{"name":"initialOwner","type":"address","internalType":"address"}],"stateMutability":"nonpayable"},{"type":"function","name":"allowance","inputs":[{"name":"owner","type":"address","internalType":"address"},{"name":"spender","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"approve","inputs":[{"name":"spender","type":"address","internalType":"address"},{"name":"value","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"nonpayable"},{"type":"function","name":"balanceOf","inputs":[{"name":"account","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"decimals","inputs":[],"outputs":[{"name":"","type":"uint8","internalType":"uint8"}],"stateMutability":"view"},{"type":"function","name":"mint","inputs":[{"name":"to","type":"address","internalType":"address"},{"name":"amount","type":"uint256","internalType":"uint256"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"name","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"owner","inputs":[],"outputs":[{"name":"","type":"address","internalType":"address"}],"stateMutability":"view"},{"type":"function","name":"renounceOwnership","inputs":[],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"symbol","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"totalSupply","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"transfer","inputs":[{"name":"to","type":"address","internalType":"address"},{"name":"value","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"nonpayable"},{"type":"function","name":"transferFrom","inputs":[{"name":"from","type":"address","internalType":"address"},{"name":"to","type":"address","internalType":"address"},{"name":"value","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"nonpayable"},{"type":"function","name":"transferOwnership","inputs":[{"name":"newOwner","type":"address","internalType":"address"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"event","name":"Approval","inputs":[{"name":"owner","type":"address","indexed":true,"internalType":"address"},{"name":"spender","type":"address","indexed":true,"internalType":"address"},{"name":"value","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"OwnershipTransferred","inputs":[{"name":"previousOwner","type":"address","indexed":true,"internalType":"address"},{"name":"newOwner","type":"address","indexed":true,"internalType":"address"}],"anonymous":false},{"type":"event","name":"Transfer","inputs":[{"name":"from","type":"address","indexed":true,"internalType":"address"},{"name":"to","type":"address","indexed":true,"internalType":"address"},{"name":"value","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"error","name":"ERC20InsufficientAllowance","inputs":[{"name":"spender","type":"address","internalType":"address"},{"name":"allowance","type":"uint256","internalType":"uint256"},{"name":"needed","type":"uint256","internalType":"uint256"}]},{"type":"error","name":"ERC20InsufficientBalance","inputs":[{"name":"sender","type":"address","internalType":"address"},{"name":"balance","type":"uint256","internalType":"uint256"},{"name":"needed","type":"uint256","internalType":"uint256"}]},{"type":"error","name":"ERC20InvalidApprover","inputs":[{"name":"approver","type":"address","internalType":"address"}]},{"type":"error","name":"ERC20InvalidReceiver","inputs":[{"name":"receiver","type":"address","internalType":"address"}]},{"type":"error","name":"ERC20InvalidSender","inputs":[{"name":"sender","type":"address","internalType":"address"}]},{"type":"error","name":"ERC20InvalidSpender","inputs":[{"name":"spender","type":"address","internalType":"address"}]},{"type":"error","name":"OwnableInvalidOwner","inputs":[{"name":"owner","type":"address","internalType":"address"}]},{"type":"error","name":"OwnableUnauthorizedAccount","inputs":[{"name":"account","type":"address","internalType":"address"}]}]};
